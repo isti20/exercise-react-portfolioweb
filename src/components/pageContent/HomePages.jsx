3290aa3a77fbfc28033caf4a29aa8aaca339f807
iis istiqomah <iisistiqomah20@gmail.com>
2023-04-10 23:23:45 +0700

    Create about component

--- src/components/pageContent/HomePages.jsx
+++ src/components/pageContent/HomePages.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "../home/homes/Home";
 import Branding from "../home/homes/Branding";
+import About from "../home/about/About";
 
 const HomePages = () => {
     return (
         <>
             <Home />
             <Branding />
+            <About />
         </>
     )
 }
