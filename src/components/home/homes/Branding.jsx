d2364ec92ee6d4e92eeb86e1e659c8dc245758d6
iis istiqomah <iisistiqomah20@gmail.com>
2023-04-10 22:43:41 +0700

    create branding component

---
+++ src/components/home/homes/Branding.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Branding = () => {
+    return (
+        <>
+            <h1>Branding</h1>
+        </>
+    )
+}
+
+export default Branding
--- src/components/pageContent/HomePages.jsx
+++ src/components/pageContent/HomePages.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "../home/homes/Home";
+import Branding from "../home/homes/Branding";
 
 const HomePages = () => {
     return (
         <>
             <Home />
+            <Branding />
         </>
     )
 }
