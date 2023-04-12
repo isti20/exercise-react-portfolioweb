6c9b31b562b6e5bfac5d9a4a8766a4f4889ac158
iis istiqomah <iisistiqomah20@gmail.com>
2023-04-12 22:02:44 +0700

    Create Service component

---
+++ src/components/home/servicess/Service.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Service = () => {
+    return (
+        <div>
+            <section className="services topMargin">
+                <div className="container">
+                    <div className="heading">
+                        <h3>My Services</h3>
+                        <h1>Interactive Services Offered Me</h1>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Service
--- src/components/pageContent/HomePages.jsx
+++ src/components/pageContent/HomePages.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "../home/homes/Home";
 import Branding from "../home/homes/Branding";
 import About from "../home/about/About";
+import Service from "../home/servicess/Service";
 
 const HomePages = () => {
     return (
@@ -9,6 +10,7 @@ const HomePages = () => {
             <Home />
             <Branding />
             <About />
+            <Service />
         </>
     )
 }
